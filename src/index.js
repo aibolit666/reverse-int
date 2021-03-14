8a659226df252e93e2aaa897d782b08273e13a1f
Pavel <pavel.priladyshev@gmail.com>
2021-03-14 19:52:17 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+    if (n<0) {
+        n = -n
+    }
+    n = n+''
+    m = []
+    for (i=0;i<=n.length;i++) {
+        m.unshift(n[i])
+    }
+    if (m[0] == 0) {
+        m.shift()
+    }
+    return Number(m.join(''))
 }
